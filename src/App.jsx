a3edc7952adcbe7199b10e177b56378de0030054
Elis West <2895786W@student.gla.ac.uk>
2026-06-15 21:51:49 +0100

    working

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,35 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  async function getData() {
+  async function getMainCard() {
     const url = "https://69b30b45e224ec066bdb55a0.mockapi.io/api/v1/cdr"
     try {
-      const response = await fetch(url)
+      const response = await fetch(url);
       if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`)
+        throw new Error(`Response status: ${response.status}`);
       }
 
-      const result = await response.json()
-      console.log(result)
+      const result = await response.json();
+      for (let i = 0; i < result.length; i++) {
+        document.getElementById('printData').innerHTML += result[i]["callerName"] +" ";
+      }
+      console.log(result[0]["callerName"]);
+
     } catch (error) {
-      console.error(error.message)
+      console.error(error.message);
     }
   }
 
+  getMainCard();
+
   return (
     <>
+      
       <h1>Hello World!</h1>
-      <button onClick={getData}>Load Data</button>
+      <h1 id = "printData">This should change</h1>
+      <button /*onClick={getMainCard}*/>Load Data</button>
     </>
   )
 }
