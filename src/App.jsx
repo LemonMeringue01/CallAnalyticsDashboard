51753a0bf19b492362e6d0549fbc4a63222c4f6e
Elis West <2895786W@student.gla.ac.uk>
2026-06-16 21:30:35 +0100

    base functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState, useEffect } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from './assets/vite.svg';
+import heroImg from './assets/hero.png';
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [data, setData] = useState([]);
+  const [sumCost, setSumCost] = useState(0);
+  const [avgDuration, setAvgDuration] = useState(0);
+  const [totalSuccess, settotalSuccess] = useState(0);
+  const [totalFail, settotalFail] = useState(0);
 
-  async function getMainCard() {
-    const url = "https://69b30b45e224ec066bdb55a0.mockapi.io/api/v1/cdr"
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
+
+  useEffect(() => {
+    const getMainCard = async () => {
+      const url = "https://69b30b45e224ec066bdb55a0.mockapi.io/api/v1/cdr";
+      try {
+        const response = await fetch(url);
+        const result = await response.json();
+        setData(result);
+
+        let total = 0;
+        for (let i = 0; i < result.length; i++) {
+          total += Number(result[i].callCost);
+        }
+        total = total.toFixed(2);
+
+        setSumCost(total);
+
+        let length = 0;
+        for (let i = 0; i<result.length; i++){
+          length += Number(result[i].callDuration);
+        }
+        length = length/result.length;
+        setAvgDuration(length);
+
+        let success = 0;
+        for (let i = 0; i<result.length; i++){
+          if(result[i].callStatus == true){
+            success+=1;
+          }
+        }
+        settotalSuccess(success);
+
+
+      } catch (err) {
+        console.error(err);
       }
+    };
 
-      const result = await response.json();
-      for (let i = 0; i < result.length; i++) {
-        document.getElementById('printData').innerHTML += result[i]["callerName"] +" ";
-      }
-      console.log(result[0]["callerName"]);
-
-    } catch (error) {
-      console.error(error.message);
-    }
-  }
-
-  getMainCard();
+    getMainCard();
+  }, []);
 
   return (
     <>
-      
       <h1>Hello World!</h1>
-      <h1 id = "printData">This should change</h1>
-      <button /*onClick={getMainCard}*/>Load Data</button>
+
+
+      <h1 id="printNames">
+        {data.map(item => item.callerName + " ")}
+      </h1>
+
+      <h1 id="printTotal">
+        {data.length}
+      </h1>
+
+      <h1 id="printCallCost">
+        {sumCost}
+      </h1>
+
+      <h1 id="printAvgDuration">
+        {avgDuration}
+      </h1>
+
+      <h1 id="printTotalSuccess">
+        {totalSuccess}
+      </h1>
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
